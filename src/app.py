42f347c435c3efdcf88440752654b6ba0460ec38
mathesh-me <matheshme679@gmail.com>
2024-04-06 14:14:42 +0530

    src/ folder is added

---
+++ src/app.py
@@ -0,0 +1,19 @@
+import boto3
+import json
+import os
+
+print('Loading function')
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+    return respond(None, res="Welcome to the world of Serverless Computing!")
